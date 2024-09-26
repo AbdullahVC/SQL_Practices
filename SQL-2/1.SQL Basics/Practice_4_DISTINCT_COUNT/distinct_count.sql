feb3b54db402445240b8c27089bd777b836f3d52
Abdullah Veysel Çoşkun <abdullahveyselcoskun@gmail.com>
2024-09-26 22:35:28 +0300

    Practice 3 eklendi

--- SQL-2/1.SQL Basics/Practice_4_DISTINCT_COUNT/distinct_count.sql
+++ SQL-2/1.SQL Basics/Practice_4_DISTINCT_COUNT/distinct_count.sql
@@ -9,7 +9,8 @@ WHERE length > 60 AND length < 75;
 
 --3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 
-SELECT * FROM film
+SELECT *
+FROM film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
 --4.customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
